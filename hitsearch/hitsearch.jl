2b9896fe56743163896ed6b9996da40a6ce98fda
Kevin Jordan <kjordan@blpc0.bl.pvt>
2022-08-10 14:39:50 -0700

    tinkering with combining hitsearch steps

--- hitsearch/hitsearch.jl
+++ hitsearch/hitsearch.jl
@@ -14,6 +14,7 @@ using HDF5
 using Downloads
 using LinearAlgebra
 using BenchmarkTools
+using Statistics
 using DopplerDriftSearch
 
 # Turn off legends in plots by default
@@ -22,24 +23,63 @@ default(legend=false)
 CUDA.allowscalar(false)
 
 # read in file
-file2open = "/home/kjordan/JuliaWork/downsamp.h5"
+file2open = "/home/kjordan/juliaNBs/hitsearch/downsamp.h5"
 h5file = h5open(file2open)
 
 # extract matrix values
 spect = h5file["data"][:,1,:]
 
 # uncomment to see original spectogram
-# heatmap(spect', yflip=true)
+#heatmap(spect', yflip=true)
 
 # produce working dataset for hitsearch development
-rates = 0:-0.1:-8
+min_drift = -0.01
+max_drift = -8
+rates = min_drift:-0.01:max_drift
 Nr = length(rates)
 fdmat = intfdr(spect, rates)
 
 # uncomment to see dedoppler output freq drift rate matrix
-heatmap(fdmat', yflip=true)
+#heatmap(fdmat', yflip=true)
 
 # Hitsearch algorithm development. 2 step iterative approach
+
 # step1: calculate median & standard deviation then find all with SNR above thresh
+
+# stats calc
+std_dev = std(fdmat)
+mdian = median(fdmat)
+
+# create arrays for needed hit identification
+mask = zeros((size(fdmat))[1], (size(fdmat))[2])
+freqs_1d_array = zeros((size(fdmat))[1])
+
+# calc window size for not counting duplicate hits
+foff = attrs(h5file["data"])["foff"]
+tsamp = attrs(h5file["data"])["tsamp"]
+num_timesteps = (size(spect))[2]
+drift_rate_resolu = (foff * 1e6) / (num_timesteps * tsamp)
+window_size = 2 * ceil(abs(max_drift) / abs(drift_rate_resolu))
+
+# iterate thru to find all matrix values above SNR thresh
+for i in 1:(size(fdmat))[1] # for each freq chan
+    for j in 1:(size(fdmat))[2] #for each trial drift rate bin
+        if (fdmat[i, j]-mdian)/std_dev > 25
+            mask[i, j] = (fdmat[i, j]-mdian)/std_dev
+            freqs_1d_array[i] = 1
+            print("hit! SNR: ")
+            print(mask[i, j])
+            print(", drift rate: ")
+            print(-j/100)
+            print(", freq index: ")
+            println(i)
+        end
+    end
+end
+
+#=for k in 10:10:22
+    println("it executed")
+end=#
+
 # setp2: iterate through all potential duplicates within max drift possible window
 
