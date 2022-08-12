2b6108acec345f305bbc6edf96babd822e7dac01
Kevin Jordan <kjordan@blpc0.bl.pvt>
2022-08-12 10:06:00 -0700

    basic 1 window top-hit-search

--- hitsearch/hitsearch.jl
+++ hitsearch/hitsearch.jl
@@ -35,12 +35,14 @@ spect = h5file["data"][:,1,:]
 # produce working dataset for hitsearch development
 min_drift = -0.01
 max_drift = -8
-rates = min_drift:-0.01:max_drift
+step = 0.01
+rates = min_drift:-(step):max_drift
 Nr = length(rates)
 fdmat = intfdr(spect, rates)
 
 # uncomment to see dedoppler output freq drift rate matrix
-#heatmap(fdmat', yflip=true)
+heatmap(fdmat', yflip=true)
+display(current())
 
 # Hitsearch algorithm development. 2 step iterative approach
 
@@ -56,26 +58,46 @@ freqs_1d_array = zeros((size(fdmat))[1])
 
 # calc window size for not counting duplicate hits
 foff = attrs(h5file["data"])["foff"]
+fch1 = attrs(h5file["data"])["fch1"]
 tsamp = attrs(h5file["data"])["tsamp"]
 num_timesteps = (size(spect))[2]
 drift_rate_resolu = (foff * 1e6) / (num_timesteps * tsamp)
 window_size = 2 * ceil(abs(max_drift) / abs(drift_rate_resolu))
 
 # iterate thru to find all matrix values above SNR thresh
+max_snr = zeros(4)
 for i in 1:(size(fdmat))[1] # for each freq chan
     for j in 1:(size(fdmat))[2] #for each trial drift rate bin
-        if (fdmat[i, j]-mdian)/std_dev > 25
-            mask[i, j] = (fdmat[i, j]-mdian)/std_dev
+        snr = (fdmat[i, j]-mdian)/std_dev
+        if snr > 25
+            mask[i, j] = snr
+            if snr >= max_snr[2]
+                global max_snr[1] = j/(max_drift/step)
+                global max_snr[2] = snr
+                global max_snr[3] = i
+            end
             freqs_1d_array[i] = 1
-            print("hit! SNR: ")
+            print("Hit! drift rate: ")
+            print(-j/(max_drift/step))
+            print(", SNR: ")
             print(mask[i, j])
-            print(", drift rate: ")
-            print(-j/100)
-            print(", freq index: ")
+            print(", freq_start: ")
+            print(fch1+foff*(i-1))
+            print(", freq_index: ")
             println(i)
         end
     end
 end
+
+println("")
+print("Top Hit! drift_rate ")
+print(max_snr[1])
+print(", SNR: ")
+print(max_snr[2])
+print(", freq_start: ")
+print(fch1+foff*(max_snr[3]-1))
+print(", freq index: ")
+print(max_snr[3])
 
 #=for k in 10:10:22
     println("it executed")
